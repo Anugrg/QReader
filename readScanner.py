2cd01fa1bcc59901f63018cf1cf877abbff21dd6
Anugrg <grg.jomle@gmail.com>
2025-02-21 16:01:02 +0700

    implement threading to handle multiple serial ports

--- readScanner.py
+++ readScanner.py
@@ -1,3 +1,4 @@
+import os
 import re
 from datetime import datetime as dt
 from tabulate import tabulate
@@ -5,9 +6,56 @@ import pandas as pd
 import json
 
 import serial
+import time
+from threading import Thread
+import logging
 
 
-def save_to_file(name, input_val):
+def show_only_info(record):
+    return record.levelname == "INFO"
+
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+
+file_handler = logging.FileHandler("app.log", mode="a", encoding="utf-8")
+
+formatter = logging.Formatter(
+    "{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+)
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+console_handler.addFilter(show_only_info)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+class ScanReaderThread(Thread):
+
+    def __init__(self, config : json):
+        Thread.__init__(self)
+        logger.debug('Starting thread ok')
+        self.config = config
+        logging.info('Listening at ' + self.config.get('COM'))
+        self.ser = serial.Serial(self.config.get('COM'), self.config.get('Baud'), timeout=self.config.get('timeout'))
+        
+    def run(self):
+        while True:
+            if self.ser.in_waiting > 0:
+                data = self.ser.readline().decode('utf-8').rstrip()
+                process_data(data)
+
+
+def save_to_file(prefix, input_val):
     scan = re.findall(r"MA.*", input_val)[0]
     data_header = ['Ref No.', 'Model', 'Packing code', 'Order no.', 'Qty']
     data = []
@@ -40,25 +88,45 @@ def save_to_file(name, input_val):
     data.append(data_row)
 
     print(tabulate(data, headers=data_header, tablefmt="grid", showindex="always"))
-    exists = True
+ 
+    exists = True  # does a file on same day exist to use
+
+    _date = dt.today().strftime('%Y_%m_%d')
+    dest_folders = os.path.join(os.getcwd(), prefix, _date, model_no)
+
     try:
-        df = pd.read_csv('disc.csv', index_col=False)
+        os.makedirs(dest_folders, exist_ok=True)
+    except Exception as e:
+        logging.error("Error while creating directory", exc_info=True)
+
+    target_file = os.path.join(dest_folders, 'MRE_QR_kanban_info.csv') 
+    try:
+        df = pd.read_csv(target_file, index_col=False)
     except:
         exists = False
+
     if not exists:
         df = pd.DataFrame(data, columns=data_header)
-        
     else:
         print('adding row')
         print(data)
         df.loc[len(df)] = data[0]
 
-    df.to_csv('disc.csv', index=False)
+    df.to_csv(target_file, index=False)
 
 
 def save_to_disk(prefix, data):
-    print(data)
-    with open(f'{prefix}', 'a+') as f:
+
+    _date = dt.today().strftime('%Y_%m_%d')
+    dest_folders = os.path.join(os.getcwd(), prefix, _date)
+    try:
+        os.makedirs(dest_folders, exist_ok=True)
+    except Exception as e:
+        logging.error("Error while creating directory", exc_info=True)
+    
+    filename = os.path.join(dest_folders, 'lot_numbers.txt')
+
+    with open(f'{filename}', 'a+') as f:
         f.write(f'{data}\n' )
 
 def serial_input(ser):
@@ -80,6 +148,32 @@ def process_data(data):
         save_to_disk('MA', data)
     
 
+def process_with_threads(conf):
+    com_info = conf.get('settings')
+    
+    try:
+        for i in range(len(com_info)):
+            
+            print(com_info[i])
+            t = ScanReaderThread(com_info[i])
+            
+            t.daemon = True
+
+            t.start()
+
+    except Exception as e:
+        logger.error("Thread creation failed", exc_info=True)
+
+    while True:
+        try:
+            logger.info("Waiting for input...")
+            time.sleep(20)
+        except KeyboardInterrupt as e:
+            print("shutting down ...")
+            break
+
+
+# test
 def process_scanned_data(config):
     print('starting barcode scanner reader...')
     is_serial = config.get('use_serial')
@@ -92,6 +186,7 @@ def process_scanned_data(config):
             else:
                 use_input()
         except KeyboardInterrupt:
+            print("shutting down...")
             if ser.is_open:
                 ser.close()
             break
@@ -101,4 +196,4 @@ if __name__=='__main__':
     with open('config.json', 'r') as f:
         config = json.load(f)
 
-    process_scanned_data(config)
+    process_with_threads(config)
